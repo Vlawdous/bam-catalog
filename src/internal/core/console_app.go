8ccea0b8d8c2704ec904bedb897abe94e37d45d6
Vladislav Komarov <vlad.komarov.2002@mail.ru>
2024-09-11 01:21:33 +0700

    wip

--- src/internal/core/console_app.go
+++ src/internal/core/console_app.go
@@ -1,35 +1,21 @@
 package core
 
 import (
-	appconfig "bam-catalog/internal/config"
-	config2 "bam-catalog/internal/core/config"
+	appconfig "bam-catalog/internal/core/config"
 	appcontainer "bam-catalog/internal/core/container"
 	"fmt"
 	"io"
 	"log"
-	"os"
-	"path/filepath"
 )
 
 type ConsoleHandlerFunc func(io io.ReadWriter) error
 
 func StartCommand() error {
-	log.Print("Collect project path")
-	projectPath, err := filepath.Abs(filepath.Dir(os.Args[0]))
-	if err != nil {
-		return err
-	}
-
-	log.Print("Initialization dotEnv files")
-	if err := appconfig.LoadDotEnv(projectPath); err != nil {
-		return err
-	}
-
 	log.Print("Collect config properties")
-	config := config2.GetConfig()
+	config, err := appconfig.GetConfig()
 
 	log.Print("Initialization container")
-	container, err := appcontainer.NewContainer(&config, &log.Logger{})
+	container, err := appcontainer.NewContainer(config, &log.Logger{})
 	if err != nil {
 		return err
 	}
